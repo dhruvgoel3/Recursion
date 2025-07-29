3dbf4b87b32952b7843ce23be5c26a35beb9ecc6
dhruvgoel3 <dhruvgoel1100@gmail.com>
2025-07-29 21:02:33 +0530

    questions

--- Recursion_2/find_power.java
+++ Recursion_2/find_power.java
@@ -3,14 +3,22 @@ package Recursion_2;
 import java.util.Scanner;
 
 public class find_power {
-    public static int findPower(int p , int q)
-    {
-        if(q==0)
-        {
+    public static int findPower(int p, int q) {
+        int smallPower = findPower(p, q / 2);
+        if (q == 0) {
             return 1;
         }
-        return findPower(p, q-1) *p;
+        // return findPower(p, q-1) *p;
+        // Alternate approach
+
+        if (q % 2 == 0) {
+            return smallPower * smallPower;
+        }
+
+        return p * smallPower * smallPower;
+
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of p :-");
@@ -19,7 +27,6 @@ public class find_power {
         int q = sc.nextInt();
         System.out.println(findPower(p, q));
 
-        
     }
-    
+
 }
---
+++ Recursion_4/print_multiples.java
@@ -0,0 +1,5 @@
+package Recursion_4;
+
+public class print_multiples {
+    
+}
